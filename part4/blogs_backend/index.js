cc1783b473a04238a3e98724793d36b7483db135
Sam <16262606+choi0617@users.noreply.github.com>
2021-05-01 10:56:04 -0400

    4.1 moved blog to its own module

--- part4/blogs_backend/index.js
+++ part4/blogs_backend/index.js
@@ -5,19 +5,12 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog")
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 const db = mongoose.connection;
---
+++ part4/blogs_backend/models/blog.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+      type: String,
+      required: true
+  },
+  author: String,
+  url: {
+      type: String,
+      required: true,
+      unique: true
+  },
+  likes: Number,
+});
+
+blogSchema.set("toJSON", {
+  transform:
+    (document, returnedObj) => {
+      returnedObj.id = returnedObj._id.toString();
+      delete returnedObj.__id;
+      delete returnedObj.__v;
+    },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
