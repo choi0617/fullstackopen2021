04b78fe67168f442f0f89bbb8d14468c46045424
Sam <16262606+choi0617@users.noreply.github.com>
2021-05-01 21:34:06 -0400

    4.4 totalLikes unit test

--- part4/blogs_backend/utils/list_helper.js
+++ part4/blogs_backend/utils/list_helper.js
@@ -1,5 +1,18 @@
-const dummy = (blog) => {
-    return 1;
-}
+const dummy = (blogs) => {
+  return 1;
+};
 
-module.exports = {dummy};
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  } 
+  else if (blogs.length === 1) {
+    return blogs[0].likes
+  }
+  else {
+    const sum = blogs.reduce((prev, cur) => prev + cur.likes, 0);
+    return sum;
+  }
+};
+
+module.exports = { dummy, totalLikes };
