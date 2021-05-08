9c49cbcf71b9c86d9c716f427212db9a38016618
Sam <16262606+choi0617@users.noreply.github.com>
2021-05-08 10:56:37 -0400

    refactored tests with describe blocks

--- part4/blogs_backend/tests/blog_api.test.js
+++ part4/blogs_backend/tests/blog_api.test.js
@@ -62,106 +62,123 @@ const initialBlogs = [
 
 beforeEach(async () => {
   await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
 
-  for (let blog of initialBlogs) {
-    let blogObj = new Blog(blog);
-    await blogObj.save();
-  }
+  // for (let blog of initialBlogs) {
+  //   let blogObj = new Blog(blog);
+  //   await blogObj.save();
+  // }
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("when there are initially some blogs saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body).toHaveLength(initialBlogs.length);
+  });
 });
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
+describe("viewing a specific blog", () => {
+  test("id field is correctly named", async () => {
+    const response = await api.get("/api/blogs");
+    console.log(response);
 
-  expect(response.body).toHaveLength(initialBlogs.length);
+    expect(response.body[0].id).toBeDefined();
+  });
+
+  test("returns 400 if id is invalid", async () => {
+    const invalidId = "00000";
+
+    await api.get(`/api/blogs/${invalidId}`).expect(400);
+  });
 });
 
-test("id field is correctly named", async () => {
-  const response = await api.get("/api/blogs");
-  console.log(response);
+describe("adding a new blog", () => {
+  test("a new blog post is added", async () => {
+    const newBlog = {
+      title: "Great developer experience",
+      author: "Hector Ramos",
+      url: "https://jestjs.io/blog/2017/01/30/a-great-developer-experience",
+      likes: 7,
+    };
 
-  expect(response.body[0].id).toBeDefined();
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+    const titles = response.body.map((r) => r.title);
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1);
+    expect(titles).toContain("Great developer experience");
+  });
+
+  test("likes default to 0", async () => {
+    const newBlog = {
+      title: "Blazing Fast Delightful Testing",
+      author: "Rick Hanlon",
+      url: "https://jestjs.io/blog/2017/01/30/a-great-developer-experience",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+    const justAddedBlog = response.body.find((b) => b.url === newBlog.url);
+
+    expect(justAddedBlog.likes).toBe(0);
+  });
+
+  test("returns 400 bad request when title or url is missing", async () => {
+    const newBlog = {
+      author: "Rick Hanlon",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+  });
 });
 
-test("a new blog post is added", async () => {
-  const newBlog = {
-    title: "Great developer experience",
-    author: "Hector Ramos",
-    url: "https://jestjs.io/blog/2017/01/30/a-great-developer-experience",
-    likes: 7,
-  };
+describe("deletion of a blog", () => {
+  test("blog post is deleted", async () => {
+    await api.delete("/api/blogs/5a422bc61b54a676234d17fc").expect(204);
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+    const response = await api.get("/api/blogs");
+    const titles = response.body.map((blog) => blog.title);
 
-  const response = await api.get("/api/blogs");
-  const titles = response.body.map((r) => r.title);
-
-  expect(response.body).toHaveLength(initialBlogs.length + 1);
-  expect(titles).toContain("Great developer experience");
+    expect(response.body).toHaveLength(initialBlogs.length - 1);
+    expect(titles).not.toContain("Type wars");
+  });
 });
 
-test("likes default to 0", async () => {
-  const newBlog = {
-    title: "Blazing Fast Delightful Testing",
-    author: "Rick Hanlon",
-    url: "https://jestjs.io/blog/2017/01/30/a-great-developer-experience",
-  };
+describe("updating a blog", () => {
+  test("blog post is updated", async () => {
+    const aBlog = await api.get("/api/blogs/5a422bc61b54a676234d17fc");
+    await api
+      .put("/api/blogs/5a422bc61b54a676234d17fc")
+      .send({
+        likes: aBlog.body.likes + 1,
+      })
+      .expect(200);
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const response = await api.get("/api/blogs");
-  const justAddedBlog = response.body.find((b) => b.url === newBlog.url);
-
-  expect(justAddedBlog.likes).toBe(0);
-});
-
-test("returns 400 bad request when title or url is missing", async () => {
-  const newBlog = {
-    author: "Rick Hanlon",
-  };
-
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(400)
-    .expect("Content-Type", /application\/json/);
-});
-
-test("blog post is deleted", async () => {
-  await api.delete("/api/blogs/5a422bc61b54a676234d17fc").expect(204);
-
-  const response = await api.get("/api/blogs");
-  const titles = response.body.map((blog) => blog.title);
-
-  expect(response.body).toHaveLength(initialBlogs.length - 1);
-  expect(titles).not.toContain("Type wars");
-});
-
-test("blog post is updated", async () => {
-  const aBlog = await api.get("/api/blogs/5a422bc61b54a676234d17fc");
-  await api
-    .put("/api/blogs/5a422bc61b54a676234d17fc")
-    .send({
-      likes: aBlog.body.likes + 1,
-    })
-    .expect(200);
-
-  const response = await api.get("/api/blogs/5a422bc61b54a676234d17fc");
-  expect(response.body.likes).toBe(aBlog.body.likes + 1);
+    const response = await api.get("/api/blogs/5a422bc61b54a676234d17fc");
+    expect(response.body.likes).toBe(aBlog.body.likes + 1);
+  });
 });
 
 describe("when there is initially one user in db", () => {
@@ -180,7 +197,7 @@ describe("when there is initially one user in db", () => {
   test("create user succeeds", async () => {
     const usersAtStart = await User.find({});
 
-    // no need for newUser = new User({}) because 
+    // no need for newUser = new User({}) because
     // you are sending newUser data as a POST request
     const newUser = {
       username: "testusername",
@@ -189,17 +206,16 @@ describe("when there is initially one user in db", () => {
     };
 
     await api
-      .post('/api/users')
+      .post("/api/users")
       .send(newUser)
       .expect(200)
-      .expect('Content-Type', /application\/json/)
-
+      .expect("Content-Type", /application\/json/);
 
     const usersAtEnd = await User.find({});
     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
 
-    const usernames = usersAtEnd.map(user => user.username)
-    expect(usernames).toContain("testusername")
+    const usernames = usersAtEnd.map((user) => user.username);
+    expect(usernames).toContain("testusername");
   });
 });
 
