ff8deb9e4d333544de1a976e37565dbf20230e8e
Sam <16262606+choi0617@users.noreply.github.com>
2021-05-18 15:56:36 -0400

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,25 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+import Togglable from "../components/Togglable"
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <Togglable buttonLabel="view">
+        <p>url: {blog.url}</p>
+        <p>likes: {blog.likes}</p>
+      </Togglable>
+    </div>
+  );
+};
 
 export default Blog;
