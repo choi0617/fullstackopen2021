f36b08235283e5a3b42dc014e832ca8d595a7059
Sam <16262606+choi0617@users.noreply.github.com>
2021-05-28 14:01:21 -0400

    refactored

--- part4/blogs_backend/app.js
+++ part4/blogs_backend/app.js
@@ -11,6 +11,7 @@ const middleware = require("./utils/middleware");
 const blogRouter = require("./controllers/blog");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const resetRouter = require("./controllers/resetRouter");
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -37,6 +38,10 @@ app.use(middleware.getTokenFrom);
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  app.use("/api/testing", resetRouter);
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
